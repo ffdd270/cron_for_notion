fb6039a38bb7f5da2383160ea44a9b78db928b3e
ffdd270 <ffdd270@gmail.com>
2021-11-08 22:23:13 +0900

    PROGRAMING-59 Cron for Notion - 지정된 규격에 따라 Notion 문서를 생성할 수 있는 기능 개발

---
+++ src/go.mod
@@ -0,0 +1,10 @@
+module github.com/ffdd270/cron_for_notion
+
+go 1.17
+
+require (
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+	golang.org/x/sys v0.0.0-20200722175500-76b94024e4b6 // indirect
+)
+
+require github.com/sevlyar/go-daemon v0.1.5
--- src/main.go
+++ src/main.go
@@ -21,15 +21,41 @@ type NotionRequestPropertiesTitle struct {
 	Text struct {
 		Content string `json:"content"`
 	} `json:"text"`
+
+	Type string `json:"type"`
 }
 
-type NotionRequestPropertiesTitles struct {
+type NotionRequestPropertiesTitleData struct {
 	Title [] NotionRequestPropertiesTitle `json:"title"`
+	Type string `json:"type"`
+}
+
+type NotionRequestPropertiesDateData struct {
+	Date struct {
+		Start string `json:"start"`
+		End   *string `json:"end"`
+	} `json:"date"`
+
+	Type string `json:"type"`
 }
 
 type NotionRequestProperties  struct {
-	Title NotionRequestPropertiesTitles `json:"title"`
+	Title NotionRequestPropertiesTitleData `json:"title"`
+	Date NotionRequestPropertiesDateData `json:"date"`
 }
+
+
+type NotionRequestTextProperty struct {
+
+}
+
+
+type NotionRequestHeadingOne struct {
+	Object string `json:"object"`
+	Type string `json:"type"`
+	Heading1 string `json:"heading_1"`
+}
+
 
 type NotionRequest struct {
 	Parent NotionRequestParent `json:"parent"`
@@ -68,6 +94,77 @@ func createRequest(databaseId string) string {
 }
 
 func main() {
+	makeNotionDocumentByJsonData()
+	// startDaemon();
+}
+
+// JSONRequestPropertyDate GoLang에서는 대문자로 스타트 시켜야 한다 =ㅁ =
+type JSONRequestPropertyDate struct {
+	Start string
+	End *string // nullable
+}
+
+type JSONRequestProperties struct {
+	Title string
+	Date JSONRequestPropertyDate
+}
+
+type JSONRequest struct {
+	Properties JSONRequestProperties
+	Text string
+}
+
+
+func createRequestFromJsonRequest( databaseId string, jsonReq JSONRequest ) string {
+	req := new(NotionRequest)
+
+	req.Parent.DatabaseId = databaseId
+	req.Properties.Title.Type = "title"
+	req.Properties.Title.Title = make([]NotionRequestPropertiesTitle, 1)
+	req.Properties.Title.Title[0].Text.Content = jsonReq.Properties.Title
+	req.Properties.Title.Title[0].Type = "text"
+
+	req.Properties.Date.Type = "date"
+	req.Properties.Date.Date.Start = jsonReq.Properties.Date.Start
+	req.Properties.Date.Date.End = jsonReq.Properties.Date.End
+
+	doc, _ := json.Marshal(req)
+
+	return string(doc)
+}
+
+func makeNotionDocumentByJsonData() {
+	if len(os.Args) != 3 {
+		fmt.Print("USAGE : API_KEY\n")
+		return
+	}
+
+	notionApiKey := os.Args[1]
+	notionDatabaseId := os.Args[2]
+
+
+	data, err := os.Open( "test.json" )
+
+	if err != nil{
+		log.Print(err)
+	}
+
+	byteValue, _ := ioutil.ReadAll(data)
+
+	var infos []JSONRequest
+	json.Unmarshal( byteValue, &infos )
+
+	for _, info := range infos {
+		reqStr := createRequestFromJsonRequest( notionDatabaseId, info  )
+		log.Print(reqStr)
+
+		sendNotionRequest( notionApiKey, notionDatabaseId, reqStr )
+	}
+
+}
+
+
+func startDaemon() {
 	cntxt := &daemon.Context{
 		PidFileName: "sample.pid",
 		PidFilePerm: 0644,
@@ -107,7 +204,6 @@ func startNotionCron() {
 }
 
 func sendNotionAPI() {
-
 	if len(os.Args) != 3 {
 		fmt.Print("USAGE : API_KEY\n")
 		return
@@ -115,10 +211,14 @@ func sendNotionAPI() {
 
 	notionApiKey := os.Args[1]
 	notionDatabaseId := os.Args[2]
+	requestStr := createRequest(notionDatabaseId)
 
+	sendNotionRequest( notionApiKey, notionDatabaseId, requestStr )
+}
+
+func sendNotionRequest(notionApiKey string, notionDatabaseId string, requestStr string) {
 	fmt.Print(notionApiKey + "\n" + notionDatabaseId + "\n")
 
-	requestStr := createRequest(notionDatabaseId)
 
 	requestClient := http.Client{}
 	req, _ := http.NewRequest("POST", "https://api.notion.com/v1/pages", bytes.NewBufferString(requestStr) )
@@ -129,6 +229,7 @@ func sendNotionAPI() {
 	resp, _ := requestClient.Do(req)
 
 	defer resp.Body.Close() // 함수가 끝날 떄 불림. 우와. 짱 신기하다.
+
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
