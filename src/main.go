ed3ef90025ef138df966e04cb393ba639ce05126
ffdd270 <ffdd270@gmail.com>
2021-11-09 01:02:17 +0900

    PROGRAMING-59 Cron for Notion - 지정된 규격에 따라 Notion 문서를 생성할 수 있는 기능 개발

--- src/main.go
+++ src/main.go
@@ -17,17 +17,52 @@ type NotionRequestParent struct {
 	DatabaseId string `json:"database_id"`
 }
 
-type NotionRequestPropertiesTitle struct {
+
+type NotionRequestPropertyLink struct {
+	Url string `json:"url"`
+	Type string `json:"type"`
+}
+
+func newNotionRequestPropertyLink(urlText string) *NotionRequestPropertyLink {
+	link := new(NotionRequestPropertyLink)
+	link.Type = "url"
+	link.Url = urlText
+
+	return link
+}
+
+type NotionRequestPropertyText struct {
 	Text struct {
 		Content string `json:"content"`
+		Link *NotionRequestPropertyLink `json:"link"`
 	} `json:"text"`
 
 	Type string `json:"type"`
 }
 
+func newNotionRequestPropertyText( text string, link *string ) NotionRequestPropertyText {
+	textProperty := NotionRequestPropertyText{ Type: "text" }
+	textProperty.Text.Content = text
+
+	if link != nil {
+		textProperty.Text.Link = newNotionRequestPropertyLink( *link )
+	}
+
+	return textProperty
+}
+
+
 type NotionRequestPropertiesTitleData struct {
-	Title [] NotionRequestPropertiesTitle `json:"title"`
+	Title [] NotionRequestPropertyText `json:"title"`
 	Type string `json:"type"`
+}
+
+type NotionRequestPropertyOption struct {
+	Name string `json:"name"`
+}
+
+type NotionRequestPropertiesOptionData struct {
+	Select NotionRequestPropertyOption `json:"select"`
 }
 
 type NotionRequestPropertiesDateData struct {
@@ -39,27 +74,70 @@ type NotionRequestPropertiesDateData struct {
 	Type string `json:"type"`
 }
 
-type NotionRequestProperties  struct {
-	Title NotionRequestPropertiesTitleData `json:"title"`
-	Date NotionRequestPropertiesDateData `json:"date"`
-}
-
-
-type NotionRequestTextProperty struct {
-
-}
-
-
-type NotionRequestHeadingOne struct {
+type NotionRequestBlockObject struct {
 	Object string `json:"object"`
 	Type string `json:"type"`
-	Heading1 string `json:"heading_1"`
 }
 
+func newNotionRequestBlockObject(typeString string) NotionRequestBlockObject {
+	block := NotionRequestBlockObject{ Type: typeString, Object: "block"}
+	return block
+}
+
+type NotionRequestTextData struct {
+	Text []NotionRequestPropertyText `json:"text"`
+}
+
+type NotionRequestHeadingOneBlock struct {
+	NotionRequestBlockObject
+	Heading1 NotionRequestTextData `json:"heading_1"`
+}
+
+func newNotionRequestHeadingOneBlock( headingText string ) NotionRequestHeadingOneBlock {
+	blockObject := newNotionRequestBlockObject("heading_1")
+	headingOne := NotionRequestHeadingOneBlock{ }
+	headingOne.Object = blockObject.Object
+	headingOne.Type = blockObject.Type
+
+	headingOne.Heading1.Text = make([]NotionRequestPropertyText, 1)
+	headingOne.Heading1.Text[0] = newNotionRequestPropertyText(headingText, nil)
+	return headingOne
+}
+
+type NotionRequestParagraphBlock struct {
+	NotionRequestBlockObject
+	Paragraph NotionRequestTextData `json:"paragraph"`
+}
+
+func newNotionRequestParagraphBlock( text string ) NotionRequestParagraphBlock {
+	blockObject := newNotionRequestBlockObject("paragraph")
+	paragraph := NotionRequestParagraphBlock{}
+	paragraph.Object = blockObject.Object
+	paragraph.Type = blockObject.Type
+
+	paragraph.Paragraph.Text =  make([]NotionRequestPropertyText, 1)
+	paragraph.Paragraph.Text[0] = newNotionRequestPropertyText(text, nil)
+	return paragraph
+}
+
+func newNotionRequestParagraphBlockWithLink( text string, link * string ) NotionRequestParagraphBlock {
+	paragraph := newNotionRequestParagraphBlock(text)
+	paragraph.Paragraph.Text[0] = newNotionRequestPropertyText( text, link )
+	return paragraph
+}
+
+
+func (block * NotionRequestParagraphBlock) addText(text string, link * string) {
+	textProperty := newNotionRequestPropertyText(text, link)
+	block.Paragraph.Text = append(block.Paragraph.Text, textProperty)
+}
+
+type NotionProperties = map[string]interface{}
 
 type NotionRequest struct {
 	Parent NotionRequestParent `json:"parent"`
-	Properties NotionRequestProperties `json:"properties"`
+	Properties NotionProperties `json:"properties"`
+	Children []interface{} `json:"children"`
 }
 
 // 위 Type들은
@@ -85,8 +163,11 @@ func createRequest(databaseId string) string {
 	request := new(NotionRequest) // key가 string, value는 {}. 뭐던.
 
 	request.Parent.DatabaseId = databaseId
-	request.Properties.Title.Title = make([]NotionRequestPropertiesTitle, 1)
-	request.Properties.Title.Title[0].Text.Content = "Type을 양산해봤어요"
+	titleProperty := NotionRequestPropertiesTitleData{}
+	titleProperty.Title = make([]NotionRequestPropertyText, 1)
+	titleProperty.Title[0] = newNotionRequestPropertyText("Type을 양산해봤어요", nil)
+
+	request.Properties["title"] = titleProperty
 
 	doc, _ := json.Marshal(request)
 
@@ -94,12 +175,46 @@ func createRequest(databaseId string) string {
 }
 
 func main() {
-	makeNotionDocumentByJsonData()
+	if len(os.Args) != 4 {
+		fmt.Print("USAGE : command API_KEY DATABASE_ID\n")
+		return
+	}
+
+	if os.Args[1] == "create" {
+		makeNotionDocumentByJsonData()
+	}
+
+	if os.Args[1] == "retrieve" {
+		notionApiKey := os.Args[2]
+		notionDatabaseId := os.Args[3]
+
+		sendNotionRetrieveRequest( notionApiKey, notionDatabaseId )
+	}
+
 	// startDaemon();
 }
 
 // JSONRequestPropertyDate GoLang에서는 대문자로 스타트 시켜야 한다 =ㅁ =
+type JSONRequestType struct {
+	Type string
+}
+
+type JSONRequestTextType struct {
+	JSONRequestType
+	Text string
+}
+
+type JsonRequestTextListDataType struct {
+	Text string
+	Link *string
+}
+
+type JsonRequestTextListType struct {
+	JSONRequestType
+	Text []JsonRequestTextListDataType
+}
 type JSONRequestPropertyDate struct {
+	JSONRequestType
 	Start string
 	End *string // nullable
 }
@@ -109,58 +224,166 @@ type JSONRequestProperties struct {
 	Date JSONRequestPropertyDate
 }
 
-type JSONRequest struct {
-	Properties JSONRequestProperties
-	Text string
+type JSONTemplate struct {
+	Properties map[string]map[string]interface{}
+	Text []map[string]interface{}
 }
 
+type JSONData struct {
+	Title string
+	Text *struct {
+		Idx int
+		Text string
+		Link *string
+	}
+}
 
-func createRequestFromJsonRequest( databaseId string, jsonReq JSONRequest ) string {
+func createRequestFromJsonRequest( databaseId string, jsonTemplate JSONTemplate, jsonData JSONData) string {
 	req := new(NotionRequest)
 
 	req.Parent.DatabaseId = databaseId
-	req.Properties.Title.Type = "title"
-	req.Properties.Title.Title = make([]NotionRequestPropertiesTitle, 1)
-	req.Properties.Title.Title[0].Text.Content = jsonReq.Properties.Title
-	req.Properties.Title.Title[0].Type = "text"
+	req.Properties = NotionProperties{}
 
-	req.Properties.Date.Type = "date"
-	req.Properties.Date.Date.Start = jsonReq.Properties.Date.Start
-	req.Properties.Date.Date.End = jsonReq.Properties.Date.End
+	for key, info := range jsonTemplate.Properties {
+		typeString := info["type"].(string)
+		jsonRequestType := JSONRequestType{typeString}
+
+		if typeString == "title" {
+			text := jsonData.Title
+			titleInfo := JSONRequestTextType{ jsonRequestType, text }
+
+			titleProperty := NotionRequestPropertiesTitleData{}
+			titleProperty.Type = "title"
+			titleProperty.Title = make([]NotionRequestPropertyText, 1)
+			titleProperty.Title[0] = newNotionRequestPropertyText(titleInfo.Text, nil)
+
+			req.Properties[key] = titleProperty
+		} else if typeString == "date"  {
+			start := info["start"].(string)
+			var end *string = nil
+
+			if info["end"] != nil {
+				end = info["end"].(*string)
+			}
+
+			dateInfo := JSONRequestPropertyDate{ jsonRequestType, start, end }
+
+			dateProperty := NotionRequestPropertiesDateData{}
+			dateProperty.Type = "date"
+			dateProperty.Date.Start = dateInfo.Start
+			dateProperty.Date.End = dateInfo.End
+
+			req.Properties[key] = dateProperty
+		} else if typeString == "select" {
+			name := info["name"].(string)
+
+			optionProperty := NotionRequestPropertiesOptionData{}
+			optionProperty.Select.Name = name
+
+			req.Properties[key] = optionProperty
+
+		}
+	}
+
+	childrenSize := len(jsonTemplate.Text)
+	req.Children = make([]interface{}, childrenSize)
+
+	for index, info := range jsonTemplate.Text {
+		typeString := info["type"].(string)
+		jsonRequestType := JSONRequestType{typeString}
+		var result interface{}
+
+		if jsonRequestType.Type == "h1" {
+			text := info["text"].(string)
+			result = newNotionRequestHeadingOneBlock( text )
+		}else if jsonRequestType.Type == "text" {
+			log.Print(info)
+
+			text := info["text"].([]interface{})
+			textFirstElement := text[0].(map[string]interface{})
+
+
+
+			firstTextString := textFirstElement["text"].(string)
+			var firstLinkString *string = nil
+
+			if  textFirstElement["link"] != nil {
+				str := textFirstElement["link"].(string)
+				firstLinkString = &str
+			}
+
+			if (jsonData.Text != nil) && (jsonData.Text.Idx == index) {
+				firstTextString = jsonData.Text.Text
+				firstLinkString = jsonData.Text.Link
+			}
+
+			paragraphBlock := newNotionRequestParagraphBlockWithLink( firstTextString, firstLinkString )
+
+			for index, textInfo := range text {
+				if index == 0 {
+					continue
+				}
+
+				textElement := textInfo.(map[string]interface{})
+
+				textString := textElement["text"].(string)
+				var linkString *string = nil
+
+				if  textElement["link"] != nil {
+					str := textElement["link"].(string)
+					linkString = &str
+				}
+
+				paragraphBlock.addText( textString, linkString )
+			}
+
+			result = paragraphBlock
+		}
+
+		req.Children[index] = result
+	}
+
 
 	doc, _ := json.Marshal(req)
 
 	return string(doc)
 }
 
-func makeNotionDocumentByJsonData() {
-	if len(os.Args) != 3 {
-		fmt.Print("USAGE : API_KEY\n")
-		return
-	}
-
-	notionApiKey := os.Args[1]
-	notionDatabaseId := os.Args[2]
-
-
-	data, err := os.Open( "test.json" )
+func getByteValue(fileName string) ([]byte, error) {
+	data, err := os.Open( fileName )
 
 	if err != nil{
 		log.Print(err)
 	}
 
-	byteValue, _ := ioutil.ReadAll(data)
+	byteValue,err := ioutil.ReadAll(data)
 
-	var infos []JSONRequest
+	return byteValue, err
+}
+
+func makeNotionDocumentByJsonData() {
+	if len(os.Args) != 4 {
+		fmt.Print("USAGE : API_KEY\n")
+		return
+	}
+
+	notionApiKey := os.Args[2]
+	notionDatabaseId := os.Args[3]
+
+	byteValue, _ := getByteValue("json_template.json")
+	var template JSONTemplate
+	json.Unmarshal( byteValue, &template )
+
+	byteValue, _ = getByteValue("test.json")
+	var infos []JSONData
 	json.Unmarshal( byteValue, &infos )
 
 	for _, info := range infos {
-		reqStr := createRequestFromJsonRequest( notionDatabaseId, info  )
+		reqStr := createRequestFromJsonRequest( notionDatabaseId, template, info )
 		log.Print(reqStr)
 
-		sendNotionRequest( notionApiKey, notionDatabaseId, reqStr )
+		sendNotionPageCreateRequest( notionApiKey, notionDatabaseId, reqStr )
 	}
-
 }
 
 
@@ -213,10 +436,10 @@ func sendNotionAPI() {
 	notionDatabaseId := os.Args[2]
 	requestStr := createRequest(notionDatabaseId)
 
-	sendNotionRequest( notionApiKey, notionDatabaseId, requestStr )
+	sendNotionPageCreateRequest( notionApiKey, notionDatabaseId, requestStr )
 }
 
-func sendNotionRequest(notionApiKey string, notionDatabaseId string, requestStr string) {
+func sendNotionPageCreateRequest(notionApiKey string, notionDatabaseId string, requestStr string) {
 	fmt.Print(notionApiKey + "\n" + notionDatabaseId + "\n")
 
 
@@ -234,5 +457,26 @@ func sendNotionRequest(notionApiKey string, notionDatabaseId string, requestStr 
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("%s\n", string(data))
+}
+
+func sendNotionRetrieveRequest(notionApiKey string, notionDatabaseId string) {
+	fmt.Print(notionApiKey + "\n" + notionDatabaseId + "\n")
+
+	requestClient := http.Client{}
+	req, _ := http.NewRequest("GET", "https://api.notion.com/v1/databases/" + notionDatabaseId, nil )
+	req.Header.Add("Authorization", "Bearer " + notionApiKey)
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Notion-Version", "2021-08-16")
+
+	resp, _ := requestClient.Do(req)
+
+	defer resp.Body.Close() // 함수가 끝날 떄 불림. 우와. 짱 신기하다.
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Printf("%s\n", string(data))
 }
